56f13111be4acc824a8405a9e47be88ce10a7293
Châu Lê <smileeee@chaus-mac-mini.speedport.ip>
2025-07-27 01:07:43 +0200

    Initial commit: ROF analysis code

---
+++ isic.py
@@ -0,0 +1,223 @@
+import os
+
+import pandas as pd
+import numpy as np
+from torch.utils.data.dataset import Dataset
+import torch
+from PIL import Image
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+transforms_ = {
+    'train': [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]),
+    ],
+    'test': [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]),
+    ]
+}
+
+
+def get_isic(data_path, split="train", preprocessing=True):
+    if split == "train":
+        transform = transforms_['train']
+    else:
+        transform = transforms_['test']
+    if not preprocessing:
+        transform = transforms.Compose([t for t in transform if not isinstance(t, transforms.Normalize)])
+    else:
+        transform = transforms.Compose(transform)
+    return ImageNetDataset(data_path, train=split == "train", transform=transform)
+
+
+class ImageNetDataset(datasets.ImageFolder):
+    def __init__(self, root, *args, validate=False, train=True, use_precomputed_labels=False,
+                 labels_path=None, transform=None, **kwargs):
+        """ImageNet root folder is expected to have two directories: train and test."""
+
+        if train and validate == train:
+            raise ValueError('Train and validate can not be True at the same time.')
+        if use_precomputed_labels and labels_path is None:
+            raise ValueError('If use_precomputed_labels=True the labels_path is necessary.')
+
+        if train:
+            root = os.path.join(root, 'train')
+        #elif validate:
+        #    root = os.path.join(root, 'val_mpeg')
+        else:
+            root = os.path.join(root, 'test')
+
+        super().__init__(root, transform=transform, *args, **kwargs)
+        self.transforms = transform
+        self.preprocessing = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+        if validate and use_precomputed_labels:
+            df = pd.read_csv(labels_path, sep='\t')
+            df.input_path = df.input_path.apply(lambda x: os.path.join(root, x))
+            mapping = dict(zip(df.input_path, df.pred_class))
+
+            self.samples = [(x[0], mapping[x[0]]) for x in self.samples]
+            self.targets = [x[1] for x in self.samples]
+
+        self.class_names = (list(CLASS_NAMES.values()))
+        self.num_classes = len(self.class_names)
+
+        #self.bird_class_ids = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 
+        #                       80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 
+        #                       99, 97, 98, 100, 127, 128, 129, 130, 132, 131, 133, 134, 135, 137, 138, 
+        #                       139, 140, 141, 142, 143, 136, 144, 145, 146]
+
+
+    def reverse_augmentation(self, data: torch.Tensor) -> torch.Tensor:
+        data = data.clone() + 0
+        mean = torch.Tensor(self.preprocessing.mean).to(data)
+        var = torch.Tensor(self.preprocessing.std).to(data)
+        data *= var[:, None, None]
+        data += mean[:, None, None]
+        return torch.multiply(data, 255)
+
+    @staticmethod
+    def class_id_name(class_id: int) -> str:
+        return CLASS_NAMES[class_id]
+
+    def get_target(self, index):
+        return self.targets[index]
+    
+
+CLASS_NAMES = {'0': 'benign',
+               '1': 'malignant'}
+
+class ISICDataset(Dataset):
+    def __init__(self, basedir, split="train", transform=None):
+        try:
+            split_i = ["train", "val", "test"].index(split)
+        except ValueError:
+            raise(f"Unknown split {split}")
+        #metadata_df = pd.read_csv(os.path.join(basedir, "metadata.csv"))
+        #metadata_df = pd.read_csv("./data/meta_4groups.csv")
+        metadata_df = pd.read_csv("raw_val_4groups.csv")
+        #metadata_df = pd.read_csv(os.path.join(basedir, "mask_overlays_metadata.csv"))
+        self.metadata_df = metadata_df[metadata_df["split"] == split_i]
+        self.basedir = basedir
+        self.transform = transform
+        self.y_array = self.metadata_df['benign_malignant'].values
+        self.p_array = self.metadata_df['patches'].values
+        self.n_classes = np.unique(self.y_array).size
+        self.confounder_array = self.metadata_df['patches'].values
+        self.n_places = np.unique(self.confounder_array).size
+        self.group_array = (self.y_array * self.n_places + self.confounder_array).astype('int')
+        self.n_groups = self.n_classes * self.n_places
+        self.group_counts = (
+                torch.arange(self.n_groups).unsqueeze(1) == torch.from_numpy(self.group_array)).sum(1).float()
+        self.y_counts = (
+                torch.arange(self.n_classes).unsqueeze(1) == torch.from_numpy(self.y_array)).sum(1).float()
+        self.p_counts = (
+                torch.arange(self.n_places).unsqueeze(1) == torch.from_numpy(self.p_array)).sum(1).float()
+        self.filename_array = self.metadata_df['isic_id'].values
+
+    def __len__(self):
+        return len(self.metadata_df)
+
+    def __getitem__(self, idx):
+        y = self.y_array[idx]
+        g = self.group_array[idx]
+        p = self.confounder_array[idx]
+        n = self.filename_array[idx]
+
+        img_path = os.path.join(self.basedir, self.filename_array[idx])
+        img = Image.open(img_path).convert('RGB')
+        if self.transform:
+            img = self.transform(img)
+        return img, y, g, p, n
+
+class MaskedISIC(Dataset):
+    def __init__(self, basedir, split="train", transform=None):
+        try:
+            split_i = ["train", "val", "test"].index(split)
+        except ValueError:
+            raise(f"Unknown split {split}")
+        #metadata_df = pd.read_csv(os.path.join(basedir, "metadata.csv"))
+        #metadata_df = pd.read_csv("./data/meta_4groups.csv")
+        #metadata_df = pd.read_csv("/home/leph/data/isic/raw_val_4groups.csv")
+        metadata_df = pd.read_csv(os.path.join(basedir, "mask_overlays_metadata.csv"))
+        self.metadata_df = metadata_df[metadata_df["split"] == split_i]
+        self.basedir = basedir
+        self.transform = transform
+        self.y_array = self.metadata_df['benign_malignant'].values
+        self.p_array = self.metadata_df['patches'].values
+        self.n_classes = np.unique(self.y_array).size
+        self.confounder_array = self.metadata_df['patches'].values
+        self.n_places = np.unique(self.confounder_array).size
+        self.group_array = (self.y_array * self.n_places + self.confounder_array).astype('int')
+        self.n_groups = self.n_classes * self.n_places
+        self.group_counts = (
+                torch.arange(self.n_groups).unsqueeze(1) == torch.from_numpy(self.group_array)).sum(1).float()
+        self.y_counts = (
+                torch.arange(self.n_classes).unsqueeze(1) == torch.from_numpy(self.y_array)).sum(1).float()
+        self.p_counts = (
+                torch.arange(self.n_places).unsqueeze(1) == torch.from_numpy(self.p_array)).sum(1).float()
+        self.filename_array = self.metadata_df['isic_id'].values
+
+    def __len__(self):
+        return len(self.metadata_df)
+
+    def __getitem__(self, idx):
+        y = self.y_array[idx]
+        g = self.group_array[idx]
+        p = self.confounder_array[idx]
+        n = self.filename_array[idx]
+
+        img_path = os.path.join(self.basedir, self.filename_array[idx])
+        img = Image.open(img_path).convert('RGB')
+        if self.transform:
+            img = self.transform(img)
+        return img, y, g, p, n
+    
+
+def get_transform(target_resolution, train, augment_data):
+    scale = 256.0 / 224.0
+
+    if (not train) or (not augment_data):
+        # Resizes the image to a slightly larger square then crops the center.
+        transform = transforms.Compose([
+            transforms.Resize((int(target_resolution[0]*scale), int(target_resolution[1]*scale))),
+            transforms.CenterCrop(target_resolution),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    else:
+        transform = transforms.Compose([
+            transforms.RandomResizedCrop(
+                target_resolution,
+                scale=(0.7, 1.0),
+                ratio=(0.75, 1.3333333333333333)),
+                #interpolation=2),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    return transform
+
+
+def get_loader(data, train, **kwargs):
+    if not train: # Validation or testing
+        shuffle = False
+        sampler = None
+    else: # Training
+        shuffle = True
+        sampler = None
+    print('data len: ',len(data))
+    loader = DataLoader(
+        data,
+        shuffle=shuffle,
+        sampler=sampler,
+        **kwargs)
+    return loader
---
+++ train_baseline.py
@@ -0,0 +1,170 @@
+import os
+import torch
+import torch.nn as nn
+import sys
+import numpy as np
+import tqdm
+from functools import partial
+import pandas as pd
+import pickle
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision.models as models
+
+from isic import get_transform, get_loader
+from isic import ISICDataset
+
+from utils import evaluate, get_y_p
+
+import random
+if __name__ == '__main__':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # Data
+    target_resolution = (224, 224)
+    train_transform = get_transform(target_resolution=target_resolution, train=True, augment_data=True)
+    test_transform = get_transform(target_resolution=target_resolution, train=False, augment_data=False)
+    trainset = ISICDataset(basedir='/Users/smileeee/Documents/Resnet18-xai/multi_groups', split="train", transform=train_transform)
+    testset = ISICDataset(basedir='/Users/smileeee/Documents/Resnet18-xai/multi_groups', split="test", transform=test_transform)
+    valset = ISICDataset(basedir='/Users/smileeee/Documents/Resnet18-xai/multi_groups', split="val", transform=test_transform)
+
+
+    loader_kwargs = {'batch_size': 64, 'num_workers': 1, 'pin_memory': True}
+    train_loader = get_loader(trainset, train=True, **loader_kwargs)
+    test_loader = get_loader(testset, train=False, **loader_kwargs)
+    get_yp_func = partial(get_y_p, n_places=trainset.n_places)
+
+    # Model
+    n_classes = trainset.n_classes
+    model = models.resnet18(pretrained=False)
+    d = model.fc.in_features
+    model.fc = torch.nn.Linear(d, 2)
+    checkpoint = torch.load('./resnet18_isic.pt', map_location=torch.device('cpu' if device == 'cpu' else device))
+    model.load_state_dict(checkpoint)
+
+    model = model.to(device)
+    # Define loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    momentum_decay = 0.9  # Set your desired momentum value
+    weight_decay = 1e-4   # Set your desired weight decay value
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=momentum_decay, weight_decay=weight_decay)
+
+
+    # Evaluate baseline model
+    #accuracy = evaluate(model, test_loader, get_yp_func)
+    #print(accuracy)
+    print("Baseline model evaluation complete.")
+    #
+    #best_acc = 0.0  # Track the best accuracy
+    #save_path = "./models/retrain_ckpt/resnet18_wb_best_baseline.pth"
+    ## Training loop
+    #for epoch in range(10):  # Number of epochs
+    #    model.train()
+    #    running_loss = 0.0
+    #    for x, y, g, p, n in train_loader:
+    #        x = x.to(device).requires_grad_()
+    #        y = y.to(device)
+    #        p = p.to(device)
+    #        outputs = model(x)
+    #        loss = criterion(outputs, y)
+    #        
+    #        optimizer.zero_grad()
+    #        loss.backward()
+    #        optimizer.step()
+    #        running_loss += loss.item()
+    #    print(f'Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}')
+    #
+    #    accuracy = evaluate(model, test_loader, get_yp_func)
+    #    print(accuracy)
+    #    # Save best model
+    #    #if accuracy['mean_accuracy'] > best_acc:
+    #    #    best_acc = accuracy['mean_accuracy']
+    #    #    torch.save(model.state_dict(), save_path)
+    #
+    #print('Training complete') 
+    # ...existing code...
+    # 1. Get baseline accuracy dict
+    baseline_result = evaluate(model, test_loader, get_yp_func)
+    print(baseline_result)
+
+    # Extract penultimate features from test set
+    def extract_penultimate_features(model, dataloader, device):
+        model.eval()
+        feats, labels_all = [], []
+        with torch.no_grad():
+            for x, y, *_ in dataloader:
+                x = x.to(device)
+                # Forward up to avgpool (second-to-last layer)
+                f = model.conv1(x)
+                f = model.bn1(f)
+                f = model.relu(f)
+                f = model.maxpool(f)
+                f = model.layer1(f)
+                f = model.layer2(f)
+                f = model.layer3(f)
+                f = model.layer4(f)
+                f = model.avgpool(f)
+                f = torch.flatten(f, 1)
+                feats.append(f.cpu())
+                labels_all.append(y)
+        features = torch.cat(feats, dim=0)
+        labels = torch.cat(labels_all, dim=0)
+        return features, labels
+
+    features, labels = extract_penultimate_features(model, test_loader, device)
+    print(f"Number of units in penultimate layer: {features.shape[1]}")
+    # 2. Rank units by L1-norm
+    l1_norms = features.abs().mean(dim=0)
+    unit_ranking = torch.argsort(l1_norms, descending=True)
+
+    # 2. ROF: Mask penultimate features, activate top-n units incrementally, collect all 6 accs
+    def rof_penultimate_eval_multiacc(features, labels, model, model_fc, unit_ranking, top_k_list, device, test_loader, get_yp_func):
+        model_fc = model_fc.to(device)
+        labels = labels.to(device)
+        acc_dict = {k: [] for k in baseline_result.keys() if "accuracy" in k}
+        for k in top_k_list:
+            mask = torch.zeros(features.shape[1])
+            mask[unit_ranking[:k]] = 1.0
+            masked_feats = features * mask
+            masked_feats = masked_feats.to(device)
+
+            # Patch model.forward to use masked features for this eval
+            orig_forward = model.forward
+            batch_start = 0
+            def new_forward(x):
+                nonlocal batch_start
+                batch_size = x.shape[0]
+                out = model_fc(masked_feats[batch_start:batch_start+batch_size])
+                batch_start += batch_size
+                return out
+            model.forward = new_forward
+
+            # Evaluate with current masking
+            batch_start = 0
+            accs = evaluate(model, test_loader, get_yp_func)
+            for key in acc_dict:
+                acc_dict[key].append(accs[key])
+
+            # Restore original forward
+            model.forward = orig_forward
+
+            if k <= 10 or k % 50 == 0 or k == features.shape[1]:
+                print(f"Top {k:3d} units activated | " + " | ".join([f"{key}: {accs[key]*100:.2f}%" for key in acc_dict]))
+        return acc_dict
+
+    top_k_list = list(range(1, features.shape[1]+1))
+    acc_curves = rof_penultimate_eval_multiacc(
+        features, labels, model, model.fc, unit_ranking, top_k_list, device, test_loader, get_yp_func
+    )
+
+    # 3. Plot all 6 accuracy curves with their baselines
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(10, 7))
+    for key in acc_curves.keys():
+        plt.plot(top_k_list, acc_curves[key], label=key)
+    plt.axhline(y=baseline_result['mean_accuracy'], color='r', linestyle='--', label='Baseline')
+    plt.xlabel('Number of Activated Units')
+    plt.ylabel('Accuracy')
+    plt.title('ROF: Accuracy vs. Number of Activated Units')
+    plt.legend()
+    plt.show()
